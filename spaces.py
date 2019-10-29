a99fe1c5dddeeee5490bc4febd9cc86a2ba3fe5d
slay <decobert.a78@gmail.com>
2019-10-29 14:13:34 +0100

    docstrings n version

--- spaces.py
+++ spaces.py
@@ -1,24 +1,36 @@
+"""
+Tool to replace underscore by spaces
+"""
+
 import argparse
 import os
 
-parser = argparse.ArgumentParser(description="Rename file")
-group = parser.add_mutually_exclusive_group()
+__version__ = '0.0.1'
 
-parser.add_argument("file", help="taking filename", type=str, nargs='+')
+def main():
+    parser = argparse.ArgumentParser(description="Rename file")
+    group = parser.add_mutually_exclusive_group()
 
-group.add_argument("-s", "--space", action="store_true")
-group.add_argument("-r", "--recursive", action="store_true")
+    parser.add_argument("file", help="taking filename", type=str, nargs='+')
 
-args = parser.parse_args()
+    group.add_argument("-s", "--space", action="store_true")
+    group.add_argument("-r", "--recursive", action="store_true")
 
-new_filename = '_'.join(args.file)
+    args = parser.parse_args()
 
-filename = ' '.join(args.file)
+    new_filename = '_'.join(args.file)
 
-if args.space:
-    new_filename = args.file[0].replace('_', ' ')
-elif args.recursive:
-    for file in args.file:
-        os.rename(file, file.replace(' ', '_'))
-else:
-    os.rename(filename, new_filename)
+    filename = ' '.join(args.file)
+
+    if args.space:
+        new_filename = args.file[0].replace('_', ' ')
+    elif args.recursive:
+        for file in args.file:
+            os.rename(file, file.replace(' ', '_'))
+    else:
+        os.rename(filename, new_filename)
+
+if __name__ == "__main__":
+    main()
+
+    
